164a49249fd8f588b9bfc3a6e0bec3c7beebcdef
rafael99TW <124179747+rafael99GD@users.noreply.github.com>
2026-01-30 16:01:16 +0100

    url videos actualizada

--- src/data/gamesData.js
+++ src/data/gamesData.js
@@ -4,7 +4,7 @@ export const featuredGames = [
     title: "FPS Game",
     description: "Sumérgete en un intenso juego de disparos en primera persona con combates dinámicos, entornos 3D detallados y mecánicas de acción realistas, desarrollado en Unity con C#.",
     image: process.env.PUBLIC_URL + "/images/fps_preview.png",
-    video: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    video: "https://youtu.be/PdYSf49JIZQ",
     githubUrl: "https://github.com/rafael99GD/game-FPS",
     technologies: ["Unity", "C#", "3D Graphics"],
     featured: true
@@ -14,7 +14,7 @@ export const featuredGames = [
     title: "Juego Impostores",
     description: "Un juego multijugador de estrategia y deducción inspirado en títulos populares, donde los jugadores asumen roles únicos y deben descubrir al impostor mientras completan tareas.",
     image: process.env.PUBLIC_URL + "/images/impostor_preview.png",
-    video: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    video: "https://youtu.be/pbstDWSoreI",
     githubUrl: "https://github.com/rafael99GD/game-Juego_Impostores",
     technologies: ["React", "Node.js", "WebSockets"],
     featured: true
@@ -24,7 +24,7 @@ export const featuredGames = [
     title: "Kirbo's Survivor",
     description: "Juego de supervivencia arcade inspirado en Vampire Survivor. Controla a Kirbo, enfrenta enemigos generados aleatoriamente, usa habilidades estratégicas y sobrevive el mayor tiempo posible en mapas procedurales.",
     image: process.env.PUBLIC_URL + "/images/kirbos_preview.png",
-    video: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    video: "https://youtu.be/VJXWgl-YCHc",
     githubUrl: "https://github.com/rafael99GD/game-KirboS_Survivor",
     technologies: ["Unity", "C#", "Procedural Generation"],
     featured: true
